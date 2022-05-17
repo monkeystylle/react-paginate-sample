4bf991e651aa5d19ebb3184b30664756d5622842
meng <monkeystylle@gmail.com>
2022-05-17 20:48:22 +0800

    react paginate sample with styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,79 @@
 import styled from 'styled-components';
 import type { NextPage } from 'next';
 import ReactPaginate from 'react-paginate';
+import { useState, useEffect } from 'react';
 
 const Home: NextPage = () => {
+  const [items, setItems] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const itemsPerPage = 10;
+  const pagesVisited = pageNumber * itemsPerPage;
+
+  useEffect(() => {
+    const getComments = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await res.json();
+      setItems(data);
+      console.log('data:', data);
+    };
+
+    getComments();
+  }, []);
+
+  const displayUsers = items
+    .slice(pagesVisited, pagesVisited + itemsPerPage)
+    .map(user => (
+      <ItemsCard key={user.id}>
+        <h5>{user.id}</h5>
+        <h5>{user.title}</h5>
+        <h6>{user.email}</h6>
+      </ItemsCard>
+    ));
+
+  const pageCount = Math.ceil(items.length / itemsPerPage);
+
+  const handlePageChange = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  console.log('items:', items);
+
   return (
     <>
-      <div className="commentBox">
-        <MyPaginate pageCount={20} onPageChange={() => alert('atut!')} />
-        <h1>---------------- </h1>
-        {/* Here the pagination component is styled thanks to Bootstrap
-        classes. See https://getbootstrap.com/docs/5.1/components/pagination */}
-        <nav aria-label="Page navigation comments" className="mt-4">
-          <ReactPaginate
-            previousLabel="previous"
-            nextLabel="next"
-            breakLabel="..."
-            breakClassName="page-item"
-            breakLinkClassName="page-link"
-            pageCount={20}
-            pageRangeDisplayed={4}
-            marginPagesDisplayed={2}
-            onPageChange={() => alert('minatut ka')}
-            containerClassName="pagination justify-content-center"
-            pageClassName="page-item"
-            pageLinkClassName="page-link"
-            previousClassName="page-item"
-            previousLinkClassName="page-link"
-            nextClassName="page-item"
-            nextLinkClassName="page-link"
-            activeClassName="active"
-            // eslint-disable-next-line no-unused-vars
-            hrefBuilder={(page, pageCount, selected) =>
-              page >= 1 && page <= pageCount ? `/page/${page}` : '#'
-            }
-            hrefAllControls
-            onClick={clickEvent => {
-              console.log('onClick', clickEvent);
-              // Return false to prevent standard page change,
-              // return false; // --> Will do nothing.
-              // return a number to choose the next page,
-              // return 4; --> Will go to page 5 (index 4)
-              // return nothing (undefined) to let standard behavior take place.
-            }}
-          />
-        </nav>
-      </div>
+      <ItemsWrapper>{displayUsers}</ItemsWrapper>
+      <PaginationWrapper>
+        <MyPaginate pageCount={pageCount} onPageChange={handlePageChange} />
+      </PaginationWrapper>
     </>
   );
 };
+
+//Item Styles
+
+const ItemsWrapper = styled.div`
+  padding: 64px;
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+`;
+const ItemsCard = styled.div`
+  border: 1px solid gray;
+  border-radius: 8px;
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+`;
+
+//Pagination Styles
+
+const PaginationWrapper = styled.div`
+  padding: 32px 0;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
 
 const MyPaginate = styled(ReactPaginate).attrs({
   // You can redifine classes here, if you want.
